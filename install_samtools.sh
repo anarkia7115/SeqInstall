4fbc22ebf2b6663ec5aed2ac0ae00ce8a69d3443
Mark Gao <marc7115@gmail.com>
2015-07-14 14:04:41 +0800

    samtools

--- install_samtools.sh
+++ install_samtools.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # install samtools
-tar -jxf $APP/software/samtools-0.1.19.tar.bz2 -C $APP/snp
-cd $APP/snp/samtools-0.1.19
+tar -jxf $APP/software/samtools-0.1.19.tar.bz2 -C /application/
+cd /application/samtools-0.1.19/
 
 make
-
-mv $APP/snp/samtools-0.1.19 /application/
