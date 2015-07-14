381bffdc5dcd6a1184c9be7c3aafa6c85bbacbd5
Mark Gao <marc7115@gmail.com>
2015-07-14 14:38:29 +0800

    samtools add c file, change make step

--- get.sh
+++ get.sh
@@ -6,6 +6,7 @@ pwd
 df -h
 aws s3 cp s3://gcbibucket/align/hg19/ $APP/align/hg19/ --recursive
 aws s3 cp s3://gcbibucket/snp/hsa_bwa_hg19_header.fa.fai $APP/snp/
+aws s3 cp s3://gcbibucket/snp/bam_readfirstline.c $APP/snp/
 #aws s3 cp s3://gcbibucket/jdk-7u79-linux-x64.tar.gz $DOW/
 #aws s3 cp s3://gcbibucket/MySQL-server-5.6.25-1.el7.x86_64.rpm $DOW/
 aws s3 cp s3://gcbibucket/Split.tar.gz $DOW/
--- install_samtools.sh
+++ install_samtools.sh
@@ -4,4 +4,10 @@
 tar -jxf $APP/software/samtools-0.1.19.tar.bz2 -C /application/
 cd /application/samtools-0.1.19/
 
+mv $APP/snp/bam_readfirstline.c /application/samtools-0.1.19/
+
 make
+gcc -c /application/samtools-0.1.19/bam_readfirstline.c -lz -lpthread
+ar r /application/samtools-0.1.19/libbam.a bam_readfirstline.o
+rm -f bam_readfirstline.o
+
